5ef529ffe1643b2cd1c2dce49448d981d2a68fab
lbgc <lbrealdeveloper@gmail.com>
2020-05-12 00:48:51 +0200

    feat: add outputs private and public subnets

---
+++ modules/vpc/outputs.tf
@@ -0,0 +1,9 @@
+output "this_private_subnet" {
+  description = "Private subnets"
+  value       = concat(aws_subnet.private.*.id, [""])
+}
+
+output "this_public_subnet" {
+  description = "Public subnets"
+  value       = concat(aws_subnet.public.*.id, [""])
+}
