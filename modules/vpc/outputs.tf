3ff0e7b74c8ece22b99dfa2de59a1f6661c606f8
lbgc <lbrealdeveloper@gmail.com>
2020-05-13 00:53:43 +0200

    fix: outputs modules

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,14 +1,14 @@
-output "this_main_vpc" {
+output "this_main_vpc_id" {
   description = "The ID of the main VPC."
   value       = element(aws_vpc.main.*.id, 0)
 }
 
-output "this_private_subnet" {
+output "this_private_subnet_ids" {
   description = "The IDs of the private subnets."
   value       = concat(aws_subnet.private.*.id)
 }
 
-output "this_public_subnet" {
+output "this_public_subnet_ids" {
   description = "The IDs of the public subnets."
   value       = concat(aws_subnet.public.*.id)
 }
