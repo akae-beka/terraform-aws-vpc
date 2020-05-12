7fde68e62af2ad0658782eb26895cd7113c59b47
lbgc <lbrealdeveloper@gmail.com>
2020-05-13 00:41:59 +0200

    feat: add main vpc ID modules

--- modules/vpc/outputs.tf
+++ modules/vpc/outputs.tf
@@ -1,9 +1,14 @@
+output "this_main_vpc" {
+  description = "The ID of the main VPC."
+  value       = element(aws_vpc.main.*.id, 0)
+}
+
 output "this_private_subnet" {
-  description = "Private subnets"
+  description = "The IDs of the private subnets."
   value       = concat(aws_subnet.private.*.id)
 }
 
 output "this_public_subnet" {
-  description = "Public subnets"
+  description = "The IDs of the public subnets."
   value       = concat(aws_subnet.public.*.id)
 }
