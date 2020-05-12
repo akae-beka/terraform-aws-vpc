98d29422f455652842035542b7a9a71d3ec35bba
lbgc <lbrealdeveloper@gmail.com>
2020-05-13 00:54:35 +0200

    fix: outputs root folder

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,14 @@
 output "vpc_id" {
   description = "The ID of the main VPC."
-  value       = module.aws_vpc.this_main_vpc
+  value       = module.aws_vpc.this_main_vpc_id
 }
 
 output "private_subnets_ids" {
   description = "The IDs of the private subnets."
-  value       = module.aws_vpc.this_private_subnet
+  value       = module.aws_vpc.this_private_subnet_ids
 }
 
 output "public_subnets_ids" {
   description = "The IDs of the public subnets."
-  value       = module.aws_vpc.this_public_subnet
+  value       = module.aws_vpc.this_public_subnet_ids
 }
