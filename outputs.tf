f42488e122a9ef4572f41f039782ed8f9268f96f
lbgc <lbrealdeveloper@gmail.com>
2020-05-13 00:52:00 +0200

    feat: add the main vpc ID output root folder

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,14 @@
-output "private_subnets" {
-  description = "Private subnets"
+output "vpc_id" {
+  description = "The ID of the main VPC."
+  value       = module.aws_vpc.this_main_vpc
+}
+
+output "private_subnets_ids" {
+  description = "The IDs of the private subnets."
   value       = module.aws_vpc.this_private_subnet
 }
 
-output "public_subnets" {
-  description = "Public subnets"
+output "public_subnets_ids" {
+  description = "The IDs of the public subnets."
   value       = module.aws_vpc.this_public_subnet
 }
