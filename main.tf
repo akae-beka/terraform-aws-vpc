11ecdb0892374389a0b9715ac839013b9764d032
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-30 02:13:02 +0100

    fix: remove deprecated parameters aws_vpc

--- main.tf
+++ main.tf
@@ -8,7 +8,5 @@ module "aws_vpc" {
   create_vpc                       = var.create
   cidr_block                       = var.cidr_block
   instance_tenancy                 = var.instance_tenancy
-  enable_classiclink               = var.enable_classiclink
-  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
 }
