0f04ecb835d5715b4c361dd0623a9ea90b4d8d1b
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-30 02:26:34 +0100

    fix: remove classiclink from aws_vpc [deprecated]

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -36,16 +36,6 @@ variable "enable_dns_support" {
   default     = true
 }
 
-variable "enable_classiclink" {
-  description = "A boolean flag to enable/disable ClassicLink for the VPC. Only valid in regions and accounts that support EC2 Classic."
-  type        = bool
-}
-
-variable "enable_classiclink_dns_support" {
-  description = "A boolean flag to enable/disable ClassicLink DNS Support for the VPC. Only valid in regions and accounts that support EC2 Classic."
-  type        = bool
-}
-
 variable "assign_generated_ipv6_cidr_block" {
   description = "Requests an Amazon-provided IPv6 CIDR block with a /56 prefix length for the VPC. You cannot specify the range of IP addresses, or the size of the CIDR block. Default is false."
   type        = bool
