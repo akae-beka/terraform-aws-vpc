90586a9f3d421905702bffdf913c001db92bf370
lbrealdev <lbrealdeveloper@gmail.com>
2022-12-30 02:23:35 +0100

    rollback classiclink arguments

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -16,6 +16,8 @@ resource "aws_vpc" "main" {
   instance_tenancy                 = var.instance_tenancy
   enable_dns_support               = var.enable_dns_support
   enable_dns_hostnames             = var.enable_dns_hostnames
+  enable_classiclink               = var.enable_classiclink
+  enable_classiclink_dns_support   = var.enable_classiclink_dns_support
   assign_generated_ipv6_cidr_block = var.assign_generated_ipv6_cidr_block
 
   tags = merge(
